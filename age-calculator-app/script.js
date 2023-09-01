fb2f73ed5ffb856eedea8401925eca20df6946f4
Chouaib-Djerdi <djerdichouaib@gmail.com>
2023-09-01 12:22:50 +0100

    Main Layout Done

---
+++ age-calculator-app/script.js
@@ -0,0 +1,9 @@
+const day_input = document.querySelector('input[name="day"]');
+const month_input = document.querySelector('input[name="month"]');
+const year_input = document.querySelector('input[name="year"]');
+
+const submit_btn = document.querySelector(".submit-btn");
+
+const days_output = document.querySelector(".days-result");
+const months_output = document.querySelector(".months-result");
+const years_output = document.querySelector(".years-result");
